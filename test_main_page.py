53747ad7dda5cb542924bf1ac96682564b7208ff
Artem Budiak <art.budiak@gmail.com>
2022-06-20 20:31:28 +0300

    created loginpage class and test for it

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,17 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
+
+def test_login_page_elements(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
