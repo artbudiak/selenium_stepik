89003e738cbba5355085ef8bb88a8558265bd1dc
Artem Budiak <art.budiak@gmail.com>
2022-06-20 20:42:46 +0300

    refactored login page test

--- test_main_page.py
+++ test_main_page.py
@@ -7,11 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
-
-def test_login_page_elements(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.open()
-    login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
