5073c6d93d89262b3eebcf6a8952d007918c382f
Artem Budiak <art.budiak@gmail.com>
2022-06-22 15:35:20 +0300

    refactored few locators and tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import math
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -32,6 +33,13 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+    
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
     
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
