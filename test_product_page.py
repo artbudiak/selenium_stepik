f16079b1f7791461546bd8af750944b788aaa36f
Artem Budiak <art.budiak@gmail.com>
2022-06-23 16:16:12 +0300

    adding more marks for tests

--- test_product_page.py
+++ test_product_page.py
@@ -22,6 +22,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_209/"
         page = ProductPage(browser, link)
@@ -36,6 +37,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -44,6 +46,8 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+# due to task offer #7 should fail
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
@@ -77,6 +81,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.should_be_success_message_disappeared()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_209/"
     page = ProductPage(browser, link)
